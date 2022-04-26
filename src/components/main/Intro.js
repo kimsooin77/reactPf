57af677d04f71c2b0d86b25a88459424fae7094c
kimsooin77 <sm6784@naver.com>
2022-04-26 15:52:28 +0900

    이미지 로딩오류 수정

--- src/components/main/Intro.js
+++ src/components/main/Intro.js
@@ -7,14 +7,13 @@ function Intro() {
     const youtube = useSelector(state => state);
     const dispatch = useDispatch();
     const vidData = youtube.youtubeReducer.youtube;
-    console.log(vidData);
 
     const api_key = "AIzaSyCx0QSN0lGvhIu9p-zYnEGvkJUvf3p_6vI";
-    const playListId = 'PL5vDjxZRg7CQQGylvYBQ4Xh5u0pL6ptdk';
+    const playListId = 'PL5vDjxZRg7CT8LqNznxeqBKRL9b93tEko';
     const num = 5;
     const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${api_key}&playlistId=${playListId}&maxResults=${num}`;
     
-    const fetchYoutube = async ()=> {
+    const fetchYoutube = async () => {
         const response = await axios
             .get(url)
             .catch(err => console.error(err))
@@ -23,7 +22,8 @@ function Intro() {
 
     useEffect(() => {
         fetchYoutube();
-    },[])
+        console.log(vidData[3].snippet.thumbnails.medium.url);
+    },[url]);
     return(
         <section id="intro">
             <div className="inner">
@@ -32,7 +32,7 @@ function Intro() {
                     {
                         vidData.map((vid,index) => {
                             return(
-                                <img key={index} src={vid.snippet.thumbnails.medium.url} />
+                                <img alt="thumbnails" key={index} src={vid.snippet.thumbnails.medium.url} />
                             )
                         })
                     }
